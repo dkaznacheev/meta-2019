87211b8d512db06faee699b90d8cdca58e529805
dkaznacheev <dmitry.kaznacheev@gmail.com>
2019-12-25 20:27:53 +0300

    Fisrt Projection completed

--- task2.rkt
+++ task2.rkt
@@ -2,6 +2,10 @@
 
 (provide main)
 
+
+(define-namespace-anchor anc)
+(define ns (namespace-anchor->namespace anc))
+
 ;-- intp
 (define (make-ctx prg input)
    (map list (cdar prg) (map (lambda (v) `',v) input)))
@@ -19,12 +23,16 @@
 (define (update-ctx asgn ctx)
   (let ([name (cadr asgn)]
         [value (run-expr (caddr asgn) ctx)])
+    (out "name" name)
+    (out "value" value)
     (cons (list name `',value) (filter (lambda (p)
            (not (equal? name (car p)))) ctx))))
 
 (define (run-expr e ctx)
-  ;(printf "~a\n~e\n\n" ctx e)
-  (eval `(let ,ctx ,e)))
+  (out "ctx" ctx)
+  (out "e" e)
+  (eval `(let ,ctx ,e) ns)
+  )
 
 
 (define (run-commands ctx commands prg)
@@ -56,10 +64,9 @@
 (define (setnth l i v)
   (set-helper '() l i v))
 
-(define tm-prg '([right 1]
-                 [write 1]))
+(define tm-prg '([0 write 1]))
 (define tape '(0 0 0 1 0 1))
-(define tm-in `(,tape ,tm-prg))
+(define tm-in `(,tm-prg ,tape))
 
 (define find_name
   '((read name namelist valuelist)
@@ -69,6 +76,39 @@
           (goto search))
     (found (return (car valuelist)))
     ))
+
+(define tm-int
+  '((read Q Right)
+    (init (:= Qtail Q) (:= Left (list)) (goto loop))
+    (loop (if (equal? Qtail (list)) end exec))
+    (exec (:= cmd (car Qtail))
+          (:= Qtail (cdr Qtail))
+          (:= op (cadr cmd))
+          (goto match1))
+    (match1 (if (equal? op 'right) exec-right match2))
+    (match2 (if (equal? op 'left) exec-left match3))
+    (match3 (if (equal? op 'write) exec-write match4))
+    (match4 (if (equal? op 'goto) exec-goto match5))
+    (match5 (if (equal? op 'if) exec-if err))
+
+    (exec-right (:= Left (cons (car Right) Left))
+                (:= Right (cdr Right))
+                (goto loop))
+    (exec-left (:= Right (cons (car Left) Right))
+               (:= Left (cdr Left))
+               (goto loop))
+    (exec-write (:= Right (cons (caddr cmd) (cdr Right)))
+                (goto loop))
+    (exec-goto (:= Qtail (memf (λ (bmd) (equal? (car bmd) (caddr cmd))) Q))
+                (goto loop))
+    (exec-if (if (equal? (caddr cmd) (car Right)) jmp loop))
+    (jmp (:= Qtail (memf (λ (bmd) (equal? (cadddr (cdr cmd)) (car bmd))) Q))
+         (goto loop))
+    
+    (err (return `unexpected ,cmd))
+    (end (return Right))
+    )
+  )
 
 (define tm `((read tape p)
              ["init" [:= ti 0]
@@ -91,7 +131,7 @@
              ["exit" (return tape)]))
 
 (define tm-div
-  '((p) (ti pi tape inst)))
+  '((p pi inst) (ti tape)))
 
 ;-- mix
 
@@ -144,12 +184,15 @@
               (cmd-loop      [if (null? bb) add-block cmd-init])
               (cmd-init      [:= command (car bb)]
                              [:= cmd (car command)]
-                             [:= tmp (out "CMD" cmd)]
                              [:= bb (cdr bb)]
                              [goto check-asgn])
               (check-asgn    [if (equal? ':= cmd) mix-asgn check-goto])
               (mix-asgn      [if (is-static division (cadr command)) s-asgn d-asgn])
-              (s-asgn        [:= vs (update-ctx (list ':= (cadr command) (reduce (caddr command) vs)) vs)]
+              (s-asgn        ;[:= tmp (out "ctx" vs)]
+                             [:= newvar (list ':= (cadr command) (reduce (caddr command) vs))]
+                             ;[:= tmp (out "ctx" vs)]
+                             ;[:= tmp (out "nv" newvar)]
+                             [:= vs (update-ctx newvar vs)]
                              [goto cmd-loop])
               (d-asgn        [:= tmp (out ""  (reduce (caddr command) vs))]
                              [:= code (append code (list (list ':= (cadr command) (reduce (caddr command) vs))))]
@@ -164,7 +207,8 @@
                              [:= ppt (caddr command)]
                              [:= ppf (cadddr command)]
                              [if (is-static-expr division expr) s-if d-if])
-              (s-if          [:= ppr (if (run-expr expr vs) ppt ppf)]
+              (s-if          ;[:= tmp (out "expr" expr)]
+                             [:= ppr (if (run-expr expr vs) ppt ppf)]
                              [:= bb (cdr (find-block ppr program))]
                              [goto cmd-loop])
               (d-if          [:= pending (append pending (new-pending marked (list ppt vs)))]
@@ -201,17 +245,18 @@
 (define (mix-and-run mix-input run-input)
   (let ([mixed (run mix mix-input)])
     (pprint mixed)
+    
     (run mixed run-input)))
 
 (define (futamura-1-naive-simple)
   (mix-and-run p-in '(3)))
 
 (define (futamura-1-naive)
-  (let* ([p-fc tm]
-         [p-div '((p) (ti pi tape inst))]
-         [p-vs0 `((p ,tm-prg))]
+  (let* ([p-fc tm-int]
+         [p-div '((Q Qtail op cmd) (Right))]
+         [p-vs0 `((Q ',tm-prg))]
          [p-in `(,p-fc ,p-div ,p-vs0)])
-  (mix-and-run p-in tape)))
+  (mix-and-run p-in (list tape))))
 
 (define (main)
-  (futamura-1-naive-simple))
+  (futamura-1-naive))
